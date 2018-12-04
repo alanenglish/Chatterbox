ec375761dfc5d0005d72c636330139a2f0e94a16
Alan English <aenglish@manorhillcapital.com>
2018-12-03 22:53:10 -0600

    create messages and users tables and models, add necessary routes, refactor sessions new view, create messages partial and display message from DB on chatroom index, update navigation menu links to conditionally render and link to appropriate views, add error and success message pop ups, add create and destroy actions to sessions controller, add helper methods in application controller, add javascript for closing of pop ups, add bcrypt and hirb to gemfile, run migrations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,36 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
+
   def new
 
   end
 
-  # def create
-  #   user = User.find_by(email: params[:session][:email].downcase)
-  #
-  #   if user && user.authenticate(params[:session][:password])
-  #     session[:user_id] = user.id
-  #     flash[:success] = "You have successfullly logged in."
-  #     redirect_to user_path(user)
-  #   else
-  #     flash.now[:danger] = "We could not log you in, please try again."
-  #     render :new
-  #   end
-  # end
-  #
-  # def destroy
-  #   session[:user_id] = nil
-  #   flash[:"success"] = "You have successfully logged out."
-  #   redirect_to root_path
-  # end
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfullly logged in."
+      redirect_to root_path
+    else
+      flash.now[:error] = "We could not log you in, please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out."
+    redirect_to root_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in."
+      redirect_to root_path
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'chatroom#index'
+  # custom login route for sessions new action - page displays login form
   get '/login' => 'sessions#new'
+  # custom login route for sesseions create action - actually signs the user into platform
+  post '/login' => 'sessions#create'
+  # custom logout action
+  delete '/logout' => 'sessions#destroy'
 end
